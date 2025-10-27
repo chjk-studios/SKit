c5016df1873947da4db9d1d54b94d4b87e331a55
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-10-27 16:28:30 +0100

    a

--- _assets/js/all.js
+++ _assets/js/all.js
@@ -9,3 +9,21 @@ if (type) {
     allTitle.textContent = "Not found..."
 }
 
+
+
+async function generateAllCards() {
+    const data = await getSkriptData()
+
+    let sorted_by_creation = data.sort().reverse()
+    let card = null
+    for (const cardData of sorted_by_creation) {
+        card = makeCard(cardData.id, cardData.name, cardData.tags, cardData.short_desc, cardData.version)
+        latestDiv.appendChild(card)
+    }
+
+    let filtered_by_featured = data.sort().filter(item => item.tags?.includes("Featured"))
+    for (const cardData of filtered_by_featured) {
+        card = makeCard(cardData.id, cardData.name, cardData.tags, cardData.short_desc, cardData.version)
+        featuredDiv.appendChild(card)
+    }
+}
