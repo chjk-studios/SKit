7f8c9e0efbd9d37d0ef86f79e53869043cd9b533
joplayxyz <joplayyt@web.de>
2025-10-28 18:05:37 +0100

    changes to styling and added detection if ?id= is in the url

--- _assets/js/skript.js
+++ _assets/js/skript.js
@@ -18,22 +18,40 @@ getSkriptData().then(data => {
 });
 
 function updateDetails(data) {
-    const createdDate = new Date(data.created);
-    const options = {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false
-    };
-    document.getElementById("details-created").innerText = createdDate.toLocaleString(undefined, options);
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    if (id) {
+        const createdDate = new Date(data.created);
+        const options = {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+        };
+        document.getElementById("details-created").innerText = createdDate.toLocaleString(undefined, options);
 
-    document.getElementById("details-name").innerText = data.name
-    document.getElementById("details-author").innerText = "By " + data.author
-    document.getElementById("details-version").innerText = data.version
-    document.getElementById("details-downloadbutton").href = data.dlink
+        document.getElementById("details-name").innerText = data.name;
+        document.getElementById("details-author").innerText = "By " + data.author;
+        document.getElementById("details-version").innerText = data.version;
+        document.getElementById("details-downloadbutton").href = data.dlink;
 
-    const descMd2Html = marked.parse(data.desc);
-    descField.innerHTML = descMd2Html;
+        document.title = `${data.name} by ${data.author}`;
+
+        const descMd2Html = marked.parse(data.desc);
+        descField.innerHTML = descMd2Html;
+        console.log(data.id);
+    } else {
+        document.getElementById("details-created").innerText = "Not Found!";
+
+        document.getElementById("details-name").innerText = "Not Found!";
+        document.getElementById("details-author").innerText = "Not Found!";
+        document.getElementById("details-version").innerText = "Not Found!";
+        descField.innerHTML = "<h3>The Skript You tried to Find was not found in the database of SKit!</h3>";
+        document.getElementById("details-downloadbutton").href = "/404.html";
+        console.log(data.id);
+    }
+    
 }
+
